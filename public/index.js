7b0135b31091bb4d69f558d288e8282cf4ebdae2
hassanqari9 <you@example.comhassanqari9@gmail.com>
2023-03-05 18:09:13 +0530

    added css & changed data tranfer method

--- public/index.js
+++ public/index.js
@@ -1,17 +1,17 @@
-document.querySelector('button').addEventListener('click',myFunction)
+// document.querySelector('button').addEventListener('click',myFunction)
 
-function myFunction(e) {
-    e.preventDefault();
+// function myFunction(e) {
+//     e.preventDefault();
 
-    const food = document.querySelector('.food').value
-    const amount = document.querySelector('.amount').value
+//     const food = document.querySelector('.food').value
+//     const amount = document.querySelector('.amount').value
     
     
-    fetch(`http://localhost:3000/search?food=${food}&amount=${amount}`).then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-        document.querySelector('h1').innerHTML=`Food: ${data.name}<br>Amount: ${data.serving_size_g}g<br>Calories: ${data.calories}cal`
-    })
-    })
-}
+//     fetch(`http://localhost:3000/search?food=${food}&amount=${amount}`).then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//         document.querySelector('h1').innerHTML=`Food: ${data.name}<br>Amount: ${data.serving_size_g}g<br>Calories: ${data.calories}cal`
+//     })
+//     })
+// }
 
--- src/nutrition.js
+++ src/nutrition.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 // keep app.js clean ✅
 // move nutrition to "nutrition.js" in "src" directory ✅
-router.get('/search', (req,res) => {
+router.get('/', (req,res) => {
+    res.render('value')
+})
 
+router.get('/search', (req,res) => {
+    
     const request = require('postman-request');
     
     const url = 'https://api.api-ninjas.com/v1/nutrition?query=';
@@ -38,7 +42,12 @@ router.get('/search', (req,res) => {
                 else
                 
                     console.log(response.body)
-                    res.send(response.body[0])
+                    // res.send(response.body[0])
+                    res.render('value',{
+                        food:`Food: ${response.body[0].name}`,
+                        amount:`Amount: ${response.body[0].serving_size_g}g`,
+                        calories:`Calories: ${response.body[0].calories}`
+                    })
                     
             }
         )
